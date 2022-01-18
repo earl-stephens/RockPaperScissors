c94a1711f9a0b8db19a952629e6475be13d41977
earl-stephens <stephensearl@hotmail.com>
2022-01-17 23:41:33 -0700

    Create a before method for GameTest

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,21 +1,32 @@
 package test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import main.Game;
 
 public class GameTest {
+	private Game game;
+	
+	@Before
+		public void setup() {
+		game = new Game();
+	}
+	
 	@Test
 	public void shouldReturnAnInstanceOfGame() {
-		Game game = new Game();
 		Assert.assertNotNull(game);
 	}
 	
 	@Test
 	public void shouldPrintWelcomeMessage() {
-		Game game = new Game();
 		String expected = "Welcome to Rock Paper Scissors!";
 		Assert.assertEquals(expected, game.welcomeMessage());
+	}
+	
+	@Test
+	public void shouldPromptForUserInput() {
+		
 	}
 }
